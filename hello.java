080d79a23d7660a27ba7f0ae78b5aef3d30ccb41
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-11-26 13:32:51 +0600

    Added a new method called greeting to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
-class A{
+public class Hello{
 public static void main(String args[]){
-system.out.println("Hello World");
+system.out.println("Vs code is Awesome");
+greeting("Tuli")
+}
+public static void grretings(String name){
+    system.out.println("welcome " + name);
 }
 }
