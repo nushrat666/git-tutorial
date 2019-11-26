eee3550a0a6f6b29bcb2ab778d78069017294150
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-11-26 16:23:13 +0600

    Add a new method called sqr

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 public static void main(String args[]){
 system.out.println("Vs code is Awesome");
-greeting("Tuli")
+greeting("Tuli");
 }
 public static void grretings(String name){
     system.out.println("welcome " + name);
