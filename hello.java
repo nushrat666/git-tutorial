79031dbf7feb8fc2a0fb8cec362633703e666c8d
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-11-27 17:37:19 +0600

    we have added one semicolon to our hello.java

--- hello.java
+++ hello.java
@@ -1,12 +1,10 @@
 public class Hello{
 public static void main(String args[]){
 system.out.println("Vs code is Awesome");
-greeting("Tuli")
+greeting("Tuli");
 }
 public static void grretings(String name){
     system.out.println("welcome " + name);
 }
-public static int sqr(int x){
-    return x*x;
-}
+
 }
