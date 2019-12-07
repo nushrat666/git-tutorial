0dc89f71a558f975eae59c6faa508ad0db2bf6d4
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-12-07 13:12:34 +0600

    all method removed and add message method

--- hello.java
+++ hello.java
@@ -2,16 +2,11 @@ public class Hello{
 public static void main(String args[]){
 system.out.println("Vs code is Awesome...");
 
-display();
+
 }
-public static void greetings(String name){
-    system.out.println("welcome " + name);
+public static void message(){
+    system.out.println("Hooray!!! Now we are able to contribute on open source...");
 }
-    public static void display(){
-        system.out.println("Hello Hello");
-}
-public static int sqr(int x){
-    return x*x;
-}
+
 
 }
